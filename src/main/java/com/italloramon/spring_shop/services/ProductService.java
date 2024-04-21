0fc20c78935088c745ebfb4f5a3cd6a889fc82a3
italloramon <itallostudy@gmail.com>
2024-04-21 14:31:26 -0300

    feat: insert product

--- src/main/java/com/italloramon/spring_shop/services/ProductService.java
+++ src/main/java/com/italloramon/spring_shop/services/ProductService.java
@@ -9,8 +9,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Service
 public class ProductService {
     @Autowired
@@ -26,5 +24,16 @@ public class ProductService {
     public ProductDTO findById(Long id) {
         Product product = repository.findById(id).get();
         return new ProductDTO(product);
+    }
+
+    @Transactional()
+    public ProductDTO insert(ProductDTO dto) {
+        Product entity = new Product();
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
+
+        return new ProductDTO(repository.save(entity));
     }
 }
