2db4e3b7e3aa8573cccac6db22f5a85bb0d6c0ac
italloramon <itallostudy@gmail.com>
2024-04-21 13:42:53 -0300

    feat: find all products paged

--- src/main/java/com/italloramon/spring_shop/controllers/ProductController.java
+++ src/main/java/com/italloramon/spring_shop/controllers/ProductController.java
@@ -3,10 +3,14 @@ package com.italloramon.spring_shop.controllers;
 import com.italloramon.spring_shop.dto.ProductDTO;
 import com.italloramon.spring_shop.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -14,6 +18,11 @@ public class ProductController {
 
     @Autowired
     private ProductService service;
+
+    @GetMapping()
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return service.findAll(pageable);
+    }
 
     @GetMapping(value = "/{id}")
     public ProductDTO findById(@PathVariable Long id) {
